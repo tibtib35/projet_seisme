c6676d47d0cf80b7341480b5b35ebfba8f9984b3
AdamiiisZboubZboub <160507647+AdamiiisZboubZboub@users.noreply.github.com>
2024-02-23 12:11:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import folium
 import geopandas
-
+import get_info as gi
 m = folium.Map(location=[ 48.1333,  -1.9667], zoom_start=5)
 seisme = geopandas.read_file(
     "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&limit=5",
     driver="GeoJSON",
 )
 
-magnitude = seisme["mag"]
-print(magnitude[1])
-
+#magnitude = seisme["mag"]
+#print(magnitude[1])
+gi.recup_info(seisme)
 m.save("map.html")
+
