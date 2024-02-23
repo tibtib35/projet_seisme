e667935138d312fc07f7f3ddeeac85394d13f6b7
AdamiiisZboubZboub <160507647+AdamiiisZboubZboub@users.noreply.github.com>
2024-02-23 12:24:47 +0100

    Update get_info.py

--- get_info.py
+++ get_info.py
@@ -1,5 +1,5 @@
 def recup_info(seisme_info):
     co = []
     for i in range (len(seisme["geometry"])):
-        co.append((seisme[i].x,seisme[i].y))
+        co.append((seisme["geometry"].x,seisme["geometry"].y))
     return co
