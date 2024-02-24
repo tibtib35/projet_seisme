88b2d4098ad8529fa8f6004260044e39e34acd5a
thibault <125139669+tibtib35@users.noreply.github.com>
2024-02-24 16:30:26 +0100

    Update map_creation.py

--- map_creation.py
+++ map_creation.py
@@ -1,19 +1,5 @@
 import folium
+def create(latitude, logitude):
+    map = folium.Map(location=[ latitude,  logitude], zoom_start=5)
+    return map
 
-m = folium.Map(location=[ 48.1333,  -1.9667], zoom_start=5)
-magnitude = 9
-radius = magnitude*5000
-folium.Circle(
-    location=[48.1333,  -1.9667],
-    radius=radius,
-    color="black",
-    weight=1,
-    fill_opacity=0.6,
-    opacity=1,
-    fill_color="green",
-    fill=False,  # gets overridden by fill_color
-    popup="{} meters".format(radius),
-    tooltip="I am in meters",
-).add_to(m)
-
-m.save("map.html")
