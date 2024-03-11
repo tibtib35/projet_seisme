efecc93476e60b95a122302cb144df301c8f11e9
LuHackx <160507212+LuHackx@users.noreply.github.com>
2024-03-11 15:44:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import map_creation as mp
 import get_info as gi
 import test as dc
 import geopandas
+from folium.plugins import Search
 
 map = mp.create(48.1333,  -1.9667)
 
@@ -17,6 +18,21 @@ latitude = info[1]
 longitude = info[2]
 
 dc.circle(magnitude,latitude,longitude,map)
+
+geo = folium.GeoJson(
+    country,
+    name="Country",
+    style_function=style_function,
+).add_to(m)
+
+countrysearch = Search(
+    layer=geo,
+    geom_type="Polygon",
+    placeholder="Search for a countrye",
+    collapsed=False,
+    search_label="name",
+    weight=3,
+).add_to(m)
 
 map.save("map.html")
 
