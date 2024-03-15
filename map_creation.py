7ff78f22655c50238c647a2551386aa5d4ffd78b
thibault <125139669+tibtib35@users.noreply.github.com>
2024-03-15 12:30:18 +0100

    Update map_creation.py

--- map_creation.py
+++ map_creation.py
@@ -1,5 +1,13 @@
+import geopandas
 import folium
 def create(latitude, logitude):
     map = folium.Map(location=[ latitude,  logitude], zoom_start=5)
     return map
 
+date = ('2001-03-10','2001-03-12')
+user = '&starttime='+date[0]+'&endtime='+date[1]
+
+seismes = geopandas.read_file(
+    "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"+user,
+    driver="GeoJSON",
+)
