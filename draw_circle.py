58fb0626aead3a84fa2d18fffcf1d40b9fd13e52
thibault <125139669+tibtib35@users.noreply.github.com>
2024-03-18 15:34:04 +0100

    Update draw_circle.py

--- draw_circle.py
+++ draw_circle.py
@@ -4,16 +4,16 @@ import math
 m = folium.Map(location=[ 48.1333,  -1.9667], zoom_start=5)
 def circle(magnitude,latitude, longitude,map):
     for i in range(len(magnitude)):
-        radius = (math.exp(magnitude[i]/1.01-0.13))*1000
-        folium.Circle(
-            location=[latitude[i],  longitude [i]],
-            radius=radius,
-            color="black",
-            weight=1,
-            fill_opacity=0.6,
-            opacity=1,
-            fill_color="red",
-            fill=False,  # gets overridden by fill_color
-            popup="magnitude de {}".format(magnitude[i]),
-            tooltip="I am in meters",
-        ).add_to(map)
+        if magnitude[i] >= 0:
+            radius = magnitude[i]*100000*math.cos(longitude[i]/180*math.pi)
+            folium.Circle(
+                location=[longitude[i],  latitude [i]],
+                radius=radius,
+                weight=1,
+                color='black',
+                fill_opacity=0.8,
+                opacity=1,
+                fill_color="#FF5F5D" if magnitude[i] > 7 else "#ED8B16" if magnitude[i] > 5  else "#FFEC5C" if magnitude[i] > 4.50 else "#146152",
+                fill=False,  # gets overridden by fill_color
+                popup="magnitude de {}".format(magnitude[i]),
+            ).add_to(map)
