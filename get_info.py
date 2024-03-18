45b74a9dd76feaae486fb0ee2d1550774b96ea10
AdamiiisZboubZboub <160507647+AdamiiisZboubZboub@users.noreply.github.com>
2024-03-18 15:38:10 +0100

    Update get_info.py
    yolo

--- get_info.py
+++ get_info.py
@@ -1,2 +1,15 @@
+import requests 
+from bs4 import BeautifulSoup
+
+
 def recup_info(seismes):
     return seismes["mag"], seismes["geometry"].x,seismes["geometry"].y
+
+
+def recup_date():
+    url = 'page.html'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    date_1 = soup.find(id = 'date')
+    date_2 = soup.find(id = 'date 2')
+    return (str(date_1), str(date_2))
