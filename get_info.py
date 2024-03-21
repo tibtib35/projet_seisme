01267daabd83be54fea216435055320adc2ddd87
AdamiiisZboubZboub <160507647+AdamiiisZboubZboub@users.noreply.github.com>
2024-03-21 22:15:25 +0100

    Update get_info.py

--- get_info.py
+++ get_info.py
@@ -3,6 +3,10 @@ def recup_info(seismes):
 
 
 def recup_date():
-    date_1 = input("Entrez la date de départ (format = an-m-j):")
-    date_2 = input("Entrez la date de fin (format = an-m-j): ")
+    date_1 = input("Entrez la date de départ (format = aaaa-mm-jj):")
+    date_2 = input("Entrez la date de fin (format = aaaa-mm-jj): ")
+    if len(date_1) != 10 or type(date_1) != str or date_1[4] != '-' or date_1[7] != '-' :
+        raise NameError('Wrong format, try again')
+    elif len(date_2) != 10 or type(date_2) != str or date_2[4] != '-' or date_2[7] != '-' :
+        raise NameError('Wrong format, try again')
     return (str(date_1), str(date_2))
