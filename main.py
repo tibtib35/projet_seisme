77a0b65bb3c55e4b727b61d8a86c0d663946e019
AdamiiisZboubZboub <160507647+AdamiiisZboubZboub@users.noreply.github.com>
2024-03-21 22:16:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import map_creation as mp
 import get_info as gi
 import draw_circle as dc
-import aspose.words as aw
 
 map = mp.create(48.1333,  -1.9667)
-
-
 seismes = mp.seismes
-
 info = gi.recup_info(seismes)
 
 magnitude = info[0]
@@ -15,31 +11,21 @@ latitude = info[1]
 longitude = info[2]
 
 dc.circle(magnitude,latitude,longitude,map)
-
-
-
 map.save("map.html")
+
 def inserer_code_html(nom_fichier, nouveau_code_html, position):
     with open(nom_fichier, 'r') as fichier:
         contenu = fichier.readlines()
-
-    # Insérer le nouveau code HTML à la position spécifiée
     contenu.insert(position, nouveau_code_html)
-
     with open(nom_fichier, 'w') as fichier:
         fichier.write(''.join(contenu))
 
-
-# Code HTML à insérer
 nouveau_code = """
 <div class='btn'>
     <button> lol <button\>
 </div>
 """
 
-# Position où insérer le nouveau code HTML (lignes numérotées à partir de 0)
-
-# Insérer le nouveau code HTML au fichier existant à la position spécifiée
 inserer_code_html('map.html', nouveau_code, 40)
 css_code = """
                 .btn{background-color:rgb(115, 205, 228);width:100%}
