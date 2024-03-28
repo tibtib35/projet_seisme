c309ae14e9ba597ebbda3c8521043be0f80c51e1
thibault <125139669+tibtib35@users.noreply.github.com>
2024-03-28 17:56:52 +0100

    Update map_creation.py

--- map_creation.py
+++ map_creation.py
@@ -2,12 +2,14 @@ import geopandas
 import folium
 import get_info as gi
 def create(latitude, logitude):
-    map = folium.Map(location=[ latitude,  logitude], zoom_start=5)
+    map = folium.Map(location=[ latitude,  logitude], zoom_start=5, control_scale=True)
     return map
 
 date = gi.recup_date()
-mi=gi.recup_mag()
-user = '&starttime='+date[0]+'&endtime='+date[1]+'&minmagnitude='+mi
+
+mag = gi.recup_mag()
+
+user = '&starttime='+date[0]+'&endtime='+date[1]+'&minmagnitude='+mag
 
 seismes = geopandas.read_file(
     "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"+user,
