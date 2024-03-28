38b7a3e6fc1e7673d4744253b1e5d2b7400411bc
AdamiiisZboubZboub <160507647+AdamiiisZboubZboub@users.noreply.github.com>
2024-03-28 21:39:06 +0100

    Update get_info.py

--- get_info.py
+++ get_info.py
@@ -3,6 +3,9 @@ def recup_info(seismes):
 
 
 def recup_date():
+    '''
+    Renvoie des inputs permettant au client de rentrer la date de début et de fin des séismes
+    '''
     date_1 = input("Entrez la date de départ (format = aaaa-mm-jj): ")
     date_2 = input("Entrez la date de fin (format = aaaa-mm-jj): ")
     if len(date_1) != 10 or type(date_1) != str or date_1[4] != '-' or date_1[7] != '-' :
@@ -12,6 +15,9 @@ def recup_date():
     return (str(date_1), str(date_2))
 
 def recup_mag():
+    '''
+    Renvoie des inputs permettant au client de rentrer la date de début et de fin des séismes
+    '''
     mag = int(input("Entrez la magnitude minimum des seismes affichés (entre 0 et 9 compris): "))
     if type(mag) != int or mag > 9 or mag < 0 :
         raise NameError('Wrong format, try again')
