7f2eba079484cf0f159c83feb3bb0d08a8f575d3
AdamiiisZboubZboub <160507647+AdamiiisZboubZboub@users.noreply.github.com>
2024-03-28 21:52:38 +0100

    Update map_creation.py

--- map_creation.py
+++ map_creation.py
@@ -2,16 +2,16 @@ import geopandas
 import folium
 import get_info as gi
 def create(latitude, logitude):
-    map = folium.Map(location=[ latitude,  logitude], zoom_start=5, control_scale=True)
+    map = folium.Map(location=[ latitude,  logitude], zoom_start=5, control_scale=True) # cration de la carte, définition de la position de départ, définition d'un zoom fois 5 au départ et ajout d'une échelle de distance sur la carte
     return map
 
-date = gi.recup_date()
+date = gi.recup_date() # récupération des dates de début et de fin demandées par l'utilisateur
 
-mag = gi.recup_mag()
+mag = gi.recup_mag() # récupération de la magnitude minimum demandée par l'utilisateur
 
 user = '&starttime='+date[0]+'&endtime='+date[1]+'&minmagnitude='+mag
 
 seismes = geopandas.read_file(
-    "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"+user,
+    "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"+user, # appel à l'API
     driver="GeoJSON",
 )
